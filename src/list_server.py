23173d4a54c15d3604533344267071095c9588e5
Leon Stümpeley <leon.stuempeley@innovaitec.de>
2026-03-18 14:29:48 +0100

    Added memory dictionaries

--- src/list_server.py
+++ src/list_server.py
@@ -2,3 +2,6 @@ from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
+# Variable dictionaries to save the data
+todo_lists = {}     # {list_id: {"name": <name>}}
+todo_entries = {}   # {entry_id: {"name": <name>, "description": <description>, "user_id": <user_id>, "list_id": <list_id>}}
